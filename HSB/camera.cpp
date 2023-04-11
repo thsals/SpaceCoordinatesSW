3cdb8eb7679601bf9f7fcccf13ca726c6317bab4
hsb05 <hansangbin47@gmail.com>
2023-04-11 17:15:14 +0900

    Update camera.cpp

--- HSB/camera.cpp
+++ HSB/camera.cpp
@@ -1,6 +1,30 @@
-// camera.cpp
-#include <stdio.h>
-int main(){
-    printf("저는 한상빈 입니다.")
-    return 0;
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int ac, char** av) {
+
+	VideoCapture cap(0);
+	
+	if (!cap.isOpened())
+	{
+		printf("Can't open the camera");
+		return -1;
+	}
+
+	Mat img;
+
+	while (1)
+	{
+		cap >> img;
+
+		imshow("camera img", img);
+
+		if (waitKey(1) == 27)
+			break;
+	}
+    print("잘 모르겠어서 복붙 하였습니다.")
+
+	return 0;
 }
