f4a44165d230cf1affc2b842b2d2a87e308cc4d5
Seo Ju Young <80999008+archi7274@users.noreply.github.com>
2023-04-11 17:01:17 +0900

    Update camera.cpp

--- myFolder/camera.cpp
+++ myFolder/camera.cpp
@@ -4,25 +4,5 @@ using namespace cv;
 using namespace std;
 
 int main(int ac, char** av) {
-
-	VideoCapture cap(0);
-
-	if (!cap.isOpened())
-	{
-		printf("카메라를 열 수 없음");
-		return 0;
-	}
-
-	Mat img;
-
-	while (1)
-	{	
-		
-		cap >> img;
-
-		imshow("카메라", img);
-		if (waitKey(1) == 27)
-			break;
-	}
 	return 0;
 }
